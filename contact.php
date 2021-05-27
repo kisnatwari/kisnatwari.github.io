f36419b9a3c631ade1011544b02365474423d21f
kisnatwari <45869481+kisnatwari@users.noreply.github.com>
2021-05-27 17:30:25 +0545

    Delete contact.php

--- contact.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$message = $_POST["message"];
-	if(mail("twarikisna@gmail.com","Message from website", "Hi, This message is sent from website","From: kisnatwari@gmail.com")){
-		echo "Message successfully sent... We will contact you shortly.. Please be patient";
-	}
- ?>
