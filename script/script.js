6811f7e3464ab7e982cbf05ffdeaab8bde73885d
kisnatwari <45869481+kisnatwari@users.noreply.github.com>
2021-05-28 14:46:24 +0545

    Update script.js

--- script/script.js
+++ script/script.js
@@ -208,3 +208,26 @@ $(document).ready(function(){
 
     })
 })
+
+
+
+
+
+function getDataUrl(img) {
+   // Create canvas
+   const canvas = document.createElement('canvas');
+   const ctx = canvas.getContext('2d');
+   // Set width and height
+   canvas.width = img.width;
+   canvas.height = img.height;
+   // Draw the image
+   ctx.drawImage(img, 0, 0);
+   return canvas.toDataURL('image/jpeg');
+}
+// Select the image
+var img = new Image();
+img.src = "images/20210220_000849.jpg";
+img.addEventListener('load', function (event) {
+   const dataUrl = getDataUrl(event.currentTarget);
+   console.log(dataUrl);
+});
