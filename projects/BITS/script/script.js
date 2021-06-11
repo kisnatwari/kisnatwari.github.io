d00fbee5fb5ed918a9431420011db5a86fa9a670
kisnatwari <45869481+kisnatwari@users.noreply.github.com>
2021-06-11 20:04:33 +0545

    Add files via upload

---
+++ projects/BITS/script/script.js
@@ -0,0 +1,75 @@
+
+//nav for small devices
+small_nav();
+
+function small_nav() {
+    var nav_btn = document.querySelector('.sm-nav-btn');
+    nav_btn.onclick = function() {
+        document.querySelector(".nav-sm, .sm-nav-con").style.transitionDuration = "0.4s";
+        var nav_con = document.querySelector(".sm-nav-con");
+        var nav_list = document.querySelector(".nav-sm");
+        nav_con.style.display = "block";
+        setTimeout(function() {
+            document.querySelector(".sm-nav-con").style.opacity = "1";
+            nav_list.style.transform = "translateX(0)";
+            document.querySelector(".nav-close-btn").onclick = function() {
+                nav_list.style.transform = "translateX(-100%)";
+                document.querySelector(".sm-nav-con").style.opacity = "0";
+                setTimeout(function() {
+                    document.querySelector(".sm-nav-con").style.display = "none";
+                }, 350)
+            };
+
+            document.querySelector(".nav-sm").onblur = function() {
+                nav_list.style.transform = "translateX(-100%)";
+                document.querySelector(".sm-nav-con").style.opacity = "0";
+                setTimeout(function() {
+                    document.querySelector(".sm-nav-con").style.display = "none";
+                }, 350)
+            }
+
+        }, 100)
+
+    }
+}
+
+decrease_nav();
+function decrease_nav(){
+    window.onscroll = function(){
+
+    }
+}
+
+window.onresize = function(){
+    document.querySelector(".asdf").innerHTML = window.innerWidth;
+    console.log(window.innerWidth);
+}
+
+
+/*window.onscroll = function(){
+    var element = document.querySelector(".fadeInUp");
+    if(element.offsetTop < (this.scrollY+window.innerHeight)){
+        element.style.top = "0px";
+        element.style.opacity = "1";
+        console.log("hello");
+    }
+}
+*/
+window.onscroll = function(){
+    var elements = document.getElementsByClassName("fadeInUp");
+    for(var i = 0; i<elements.length; i++){
+        fadeInUp(elements[i]);
+    }
+    if(this.scrollY > 150)
+        document.querySelector(".nav").style.height = "65px";
+    else
+        document.querySelector(".nav").style.height = "80px";
+    increase_num();
+}
+function fadeInUp(element){
+    var scrollY = window.scrollY+(window.innerHeight);
+    if((element.offsetTop < window.scrollY+window.innerHeight)  && (element.offsetTop > window.scrollY)){
+        element.style.opacity = 1;
+        element.style.top = 0;
+    }
+}
